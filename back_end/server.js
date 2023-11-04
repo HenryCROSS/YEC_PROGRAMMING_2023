cfc1eca86af856c9a7be1b769e729451871ef3d8
YuxinS0ng <126999549+YuxinS0ng@users.noreply.github.com>
2023-11-04 16:44:32 -0400

    Update server.js

--- back_end/server.js
+++ back_end/server.js
@@ -5,6 +5,22 @@ const app = express();
 
 // Read the file from the local device
 const userPath='./student_info.json';
+const userType='./user_info.json';
+
+app.get('/login_type',function(req,res){
+    fs.readFile(userType,'utf-8',function(err,data){
+        if (err) {
+            console.error('Can not read Json file', err);
+            res.send('Error!');
+            return;
+          }
+
+          const obj=JSON.parse(data);
+          res.send(obj.student);
+          
+    })
+})
+
 
 app.get('/login_id', function(req, res) {
   fs.readFile(userPath, 'utf8', function(err, data) {
@@ -14,12 +30,34 @@ app.get('/login_id', function(req, res) {
         return;
       }
 
-    const data = JSON.parse(data);
-  
+    const obj = JSON.parse(data);
+    if(obj.id===req.id){
+        res.send(obj.student.student1);
+    }
       
    
   });
 });
+
+
+app.get('/allCourse', function(req, res) {
+    fs.readFile(userPath, 'utf8', function(err, data) {
+      if (err) {
+          console.error('Can not read Json file', err);
+          res.send('Error!');
+          return;
+        }
+  
+      const obj = JSON.parse(data);
+      if(obj.id===req.id){
+          res.send(obj.student.student1.allCourse);
+      }
+        
+     
+    });
+  });
+  
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
