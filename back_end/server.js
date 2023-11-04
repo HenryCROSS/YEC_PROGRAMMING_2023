35abf7aaa7e2a2379e49f4dd10163736222f9404
YuxinS0ng <126999549+YuxinS0ng@users.noreply.github.com>
2023-11-04 16:52:44 -0400

    Update server.js

--- back_end/server.js
+++ back_end/server.js
@@ -7,6 +7,7 @@ const app = express();
 const userPath='./student_info.json';
 const userType='./user_info.json';
 
+//Get the tpye of user and send the user info from user_info.json
 app.get('/login_type',function(req,res){
     fs.readFile(userType,'utf-8',function(err,data){
         if (err) {
@@ -22,6 +23,7 @@ app.get('/login_type',function(req,res){
 })
 
 
+//Get the id from user, then send the special info to user 
 app.get('/login_id', function(req, res) {
   fs.readFile(userPath, 'utf8', function(err, data) {
     if (err) {
@@ -40,6 +42,7 @@ app.get('/login_id', function(req, res) {
 });
 
 
+//After get the user, then send the whole courses' info from json
 app.get('/allCourse', function(req, res) {
     fs.readFile(userPath, 'utf8', function(err, data) {
       if (err) {
@@ -56,8 +59,25 @@ app.get('/allCourse', function(req, res) {
      
     });
   });
+
+//send the indivial course info to user
+app.get('/indivialCourse', function(req, res) {
+    fs.readFile(userPath, 'utf8', function(err, data) {
+      if (err) {
+          console.error('Can not read Json file', err);
+          res.send('Error!');
+          return;
+        }
   
+      const obj = JSON.parse(data);
+      if(obj.course.name===req.course.name){
+          res.send(obj.student.student1.course1);
+      }
+        
+     
+    });
+  });
+//
 
-
-
+//in the terminal, console the running status
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
