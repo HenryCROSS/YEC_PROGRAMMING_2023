50c3d4c5b934446e1de69abb2a9f368b4ad236af
Yuxin <yxsong09@gmail.com>
2023-11-04 15:22:04 -0400

    update

--- back_end/server.js
+++ back_end/server.js
@@ -5,27 +5,57 @@ const PORT = 3000;
 const app = express();
 
 // Read the file from the local device
-const coursePath = './user_info.json';
+const userPath = './user_info.json';
+const coursePath='./course_info.json';
 
-app.get('/login/type', (req, res) => {
-  fs.readFile(coursePath, 'utf8', (err, data) => {
+app.get('/login/id', (req, res) => {
+  fs.readFile(userPath, 'utf8', (err, data) => {
     if (err) {
       console.error('Can not read Json file', err);
-      res.status(500).send('Server Error');
+      res.send('Error');
       return;
     }
 
     const jsonObject = JSON.parse(data);
-    const user = req.params.user;
 
- 
-    if (user === "student" || type === "admit"||user === "educator") {
-      const userData = jsonObject[user];
-      res.json(userData);
-    } else {
-      res.status(404).send('No such a user!');
-    }
+    const id =data.users.id.find(function(obj) {
+        if (obj.id === req.id ) {
+            const userData = jsonObject[user];
+            res.json(userData);
+        } else {
+          res.json({success:false});
+        }
+      });
+   
   });
 });
+
+app.get('/login/id', (req, res) => {
+    fs.readFile(userPath, 'utf8', (err, data) => {
+        
+          
+        });
+  });
+
+app.get('/allCourse', (req, res) => {
+    fs.readFile(coursePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Can not read Json file', err);
+        res.send('Error');
+        return;
+      }
+  
+      const jsonObject = JSON.parse(data);
+      const course = req.params.course;
+  
+   
+      if (user === "student" || type === "admit"||user === "educator") {
+        const userData = jsonObject[user];
+        res.json(userData);
+      } else {
+        res.status(404).send('No such a user!');
+      }
+    });
+  });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
